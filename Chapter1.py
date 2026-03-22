44ab5000194c95627dea42a380643647fe41717b
PriyaPandey19 <pp479021@gmail.com>
2026-03-22 19:52:05 +0530

    python

--- Chapter1.py
+++ Chapter1.py
@@ -1,7 +1,14 @@
-
+#this is first python code
 
 name = "Priya "
 print("Actual Value",name)
 
 username = input("Enter your name:")
 print("Hello", username)
+
+#calculation the area of circle
+
+radius = float(input("Enter thr radius:"))
+pie = 3.14
+area = pie * radius * radius
+print("The area of circle", area)
