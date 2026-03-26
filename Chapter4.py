2000c1b58f92108cf93de7bc197958850c9b8ed3
PriyaPandey19 <pp479021@gmail.com>
2026-03-26 20:04:18 +0530

    python

---
+++ Chapter4.py
@@ -0,0 +1,18 @@
+#conditional statment
+marks = int(input("Enter the marks :"))
+
+if(marks > 0):
+    print("POSITIVE")
+elif(marks == 0):
+    print("ZERO")
+else:
+    print("NEGATIVE")     
+
+
+#lists in python
+food =["chole bathure", "choco waffle","mango", "apple", "gulab jamun"]
+print(len(food))
+
+print("First Value of the last:", food[0])
+print("Third Value of the list:", food[3])
+#hw
