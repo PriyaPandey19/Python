e84dcf36cc5747d837e7ce60c728f1fdac6ed77c
PriyaPandey19 <pp479021@gmail.com>
2026-03-29 20:26:19 +0530

    python

--- Chpter6.py
+++ Chpter6.py
@@ -4,6 +4,25 @@ num = 1
 
 while num<=10:
     print(num)
-    num = num+1
+    num += 1
 
 print("Now we are out of moves...")  
+
+#to cprint the even number
+i = 1
+while(i<=50):
+    if(i%2 == 0):
+        print(i)
+    i+=1    
+
+
+#to find the sum of natural number
+n = int(input("Enter the natural number."))
+sum = 0
+
+while n>=1:
+    sum = sum+n
+    n=n-1
+
+print("Sum=", sum)
+print("n= ",n)    
