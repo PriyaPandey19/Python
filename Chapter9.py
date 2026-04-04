89f22bce7661c6e291c1b82acb5e8f1b8a7fa244
PriyaPandey19 <pp479021@gmail.com>
2026-04-04 19:26:06 +0530

    python

--- Chapter9.py
+++ Chapter9.py
@@ -6,4 +6,32 @@ class Vehicle:
 car = Vehicle()
 print(car.color)
 bike = Vehicle()
-print(bike.milege)    
+print(bike.milege)  
+
+
+#create 2 object with diffrent vaLUEs
+
+class Laptop:
+    brand="default"
+    RAM="default 3GB"
+    price= "default"
+
+Laptop1 = Laptop()
+Laptop1.brand="Macbook"
+
+Laptop2 = Laptop()
+Laptop2.brand="Dell"
+
+print("Laptop 1 :", Laptop1.brand)
+print("Laptop 2 :", Laptop2.brand)
+
+#init constructor
+class Student:
+    schoolName ="ABE School"
+
+    def __init__(self):
+        print("Whenever a new object is created I am called automatically")
+
+student1 = Student();
+student2 = Student();        
+
