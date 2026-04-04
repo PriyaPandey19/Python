159862b849d9b0ef33a835e94af75dd54afcf869
PriyaPandey19 <pp479021@gmail.com>
2026-04-04 19:59:36 +0530

    python

--- Chapter9.py
+++ Chapter9.py
@@ -29,9 +29,31 @@ print("Laptop 2 :", Laptop2.brand)
 class Student:
     schoolName ="ABE School"
 
-    def __init__(self):
-        print("Whenever a new object is created I am called automatically")
+    def __init__(self,name, course):
+        self.name = name
+        self.course = course
 
-student1 = Student();
-student2 = Student();        
+student1 = Student("khushi", "BTech")
+print("Studen1 Name-", student1.name)
+student2 = Student("Ankit", "BCom")
+print("Studen2 Name-", student2.name)
+
+#average of marks
+class People:
+    def __init__(self,name, listOfMarks):
+        self.name = name
+        self.listOfMarks = listOfMarks
+
+    def average(self):
+        sum=0
+        for eachValue in self.listOfMarks:
+            sum = sum+eachValue
+
+        average = sum /3
+        print("Average is:", average)
+
+student1 = People("Aditya",[80,89,89])
+student1.average()
+
+
 
